b42080f6a46dd52b2067e96ffa32d6cd0056e821
0racularGh0st <nigel4tariang@gmail.com>
2020-12-09 22:58:13 +0530

    Header changes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,9 @@ import "./Header.css";
 const Header = () => {
     return (
         <div className="service-status-header">
-            <img src="../images/logo.png" height="40" width="40"/>
+            <img src="../images/logo.png" height="30" width="30"/>
             <h1 className="service-status-heading">
-                Service Status Checker
+                Service Status
             </h1>
         </div>
     )
