68a26e346c0efc840d23c11d740633a4d7fbc64b
0racularGh0st <nigel4tariang@gmail.com>
2020-12-09 21:07:04 +0530

    Add config

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Home from './Home';
 import './App.css';
 
 const App = () => {
     return (
         <div className="main-container">
-           Hello
+           <Home/>
         </div>
     );
 }
---
+++ client/src/config/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+    services: [
+        {
+            name: "Nigel David API",
+            key: "nigel_david_api",
+            host: "https://api.nigeldavid.in",
+            location: "OVH Cloud VPS"
+        },
+        {
+            name: "Nigel David Portfolio",
+            key: "nigel_david_portfolio",
+            host: "https://nigeldavid.in",
+            location: "Firebase"
+        },
+        {
+            name: "Dummy Site",
+            key: "dummy",
+            host: "https://daDad.nigeldavid.in",
+            location: "Dummy"
+        }
+    ]
+}
