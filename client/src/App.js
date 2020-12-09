ef8f905860d9dddac5a8c916c5b138a235fc58d2
0racularGh0st <nigel4tariang@gmail.com>
2020-12-09 20:08:49 +0530

    Add client initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="main-container">
+           Hello
+        </div>
+    );
+}
+
+export default App;
