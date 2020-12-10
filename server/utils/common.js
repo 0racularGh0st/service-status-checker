7c97e1af4f9bdad8e24e0761ffcb40e0591eaa3f
0racularGh0st <nigel4tariang@gmail.com>
2020-12-10 23:18:56 +0530

    Dont send notifiction when service is healthy

--- server/utils/common.js
+++ server/utils/common.js
@@ -27,8 +27,9 @@ function runScheduledCheck(){
     checkStatus(service.host)
     .then((statuscode) => {
         if(statuscode === 200){
-            let message = `${service.name} is healthy`;
-            subs.forEach(sub => { push.sendNotification(sub, message) });
+            //don't do anything it service is healthy
+            // let message = `${service.name} is healthy`;
+            // subs.forEach(sub => { push.sendNotification(sub, message) });
         }
         else{
             let message = `${service.name} is unhealthy`;
